92696cc8a8d0dde9df4199491e331be7adfcaac7
MattB859 <mateus756@hotmail.com>
2022-03-08 20:07:44 +0000

    Added functional component to toggle button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting.js';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting/>
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" />
-      <StatefulGreeting greeting="from the stateful class Component"/>
+      <NavBar/>
+      <StatefulGreeting greeting="Matthew"/>
     </div>
   );
 }
